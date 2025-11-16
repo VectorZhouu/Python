f90bb4fd28d9e7fb7a67bcfda1798e24a42fa78a
VectorZhouu <yuanzhou0000@outlook.com>
2025-11-16 15:41:05 +0800

    创建 TaylorAnalyses.py

---
+++ TaylorAnalyses.py
@@ -0,0 +1,26 @@
+# Created on iPad.
+import numpy as np
+import math
+#计算n级泰勒公式的误差率
+def factorial(num):
+    if num == 1 or num == 0:
+        return 1
+    else:
+        return num * factorial(num - 1)
+#平均误差
+def err(ob,pre,distance):
+    return np.abs(ob - pre)/distance
+obfun		= lambda x:np.e**x
+error_edgeL = int(input("请输入函数范围的左端点"))
+error_edgeR = int(input("请输入函数范围的右端点"))
+distance 	= np.abs(error_edgeR - error_edgeL)
+n 			= int(input("展开最高次数"))
+sum   = 0
+exact = 0
+for x in range(error_edgeL,error_edgeR+1):
+    exact = obfun(x) + exact
+    tlfun = 1
+    for i in range(1,n+1):
+    		tlfun += x**i / factorial(i)
+    sum += tlfun
+print("平均误差为"err(sum,exact,distance))
